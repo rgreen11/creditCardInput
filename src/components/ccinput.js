7f57d5f3a62d0d73cb0ac18b97708683e20857a9
Richard Green <richardgreen@pursuit.org>
2019-07-17 23:44:57 -0400

    created an input that handles spacing for master, visa and amex

--- src/components/ccinput.js
+++ src/components/ccinput.js
@@ -14,24 +14,63 @@ handleCardType = (e) => {
     const input = e.target.value;
     let card = '';
     let firstInt = parseInt(input[0])
-
-    if(firstInt === 4){
+    if(firstInt === 3){
+        card = 'American Express';
+        if(input.length === 19) return;
+        this.setState({output: input, card:card});
+        this.handleAmex(input)
+    } else if(firstInt === 4){
         card = 'Visa Card';
-        console.log('input 2', input[0]);
+        if(input.length === 20) return;
+        this.setState({output: input, card:card});
+        this.handleSpace(input)
     } else if (firstInt === 5){
         card = 'Master Card';
-    } 
-
+        if(input.length === 20) return;
         this.setState({output: input, card:card});
-       
+        this.handleSpace(input)
+    } 
 }
 
+handleSpace = (input) => {
+    const space = ' ';
+    console.log(input.length)
+    if(input.length === 4){
+        let newInput = input + space;
+        this.setState({output: newInput})
+        return;
+    } else if(input.length === 9){
+        let newInput = input + space;
+        this.setState({output: newInput})
+        return;
+    } else if(input.length === 14){
+        let newInput = input + space;
+        this.setState({output: newInput})
+        return;
+    } 
+}
 
+handleAmex = (input) => {
+    const space = ' ';
+    let firstInt = parseInt(input[0])
+    if(firstInt === 3){
+        this.setState({card: 'American Express'})
+    }
+    if(input.length === 4){
+        let newInput = input + space;
+        this.setState({output: newInput})
+        return;
+    } else if(input.length === 12){
+        let newInput = input + space;
+        this.setState({output: newInput})
+        return;
+    } 
+}
 
 
     render(){
         const {output, card} = this.state
-        console.log('card', card)
+
     return( <>
                 <h1>Card Type: {card}</h1>
                 <input 
